9a4751eeb2bc94abb90342ffa61e126c94126ef8
Dennis Hermansen <dennis@chosting.dk>
2023-06-08 12:24:07 +0200

    Small cleanup

--- lib/PasswordGenerator.php
+++ lib/PasswordGenerator.php
@@ -2,6 +2,7 @@
 
 namespace WHMCS\Module\Addon\PasswordChange;
 
+use Exception;
 use WHMCS\User\User;
 
 class PasswordGenerator
@@ -10,9 +11,12 @@ class PasswordGenerator
     protected int $userId;
     protected $userModel;
 
+    /**
+     * @throws Exception
+     */
     public function __construct($userId){
         if(!isset($_SESSION['adminid'])){
-            throw new \Exception('Not authenticated as admin');
+            throw new Exception('Not authenticated as admin');
         }
         $this->userId = $userId;
         $this->userModel = User::findOrFail($userId);
@@ -27,7 +31,8 @@ class PasswordGenerator
         return $this->hash;
     }
 
-    protected function store(){
+    protected function store(): void
+    {
         $this->userModel->password = $this->hash;
         $this->userModel->save();
     }
